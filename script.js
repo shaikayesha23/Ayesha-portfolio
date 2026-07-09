af374049088766eb01e5b91f6797ed283c40d21c
Shaik Ayesha <shaikayesha17865@gmail.com>
2026-07-09 21:20:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ let letter = "";
     }
 
 })();
+// Scroll Animation
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
