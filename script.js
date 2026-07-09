21d23707e263325ba1aa4eba02012e9c620921fe
Shaik Ayesha <shaikayesha17865@gmail.com>
2026-07-09 21:13:01 +0530

    Implement typing effect for portfolio text
    Added a typing effect for displaying text on the portfolio.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Portfolio Loaded Successfully");
+const text = [
+    "Aspiring Software Engineer",
+    "Java Developer",
+    "Python Developer",
+    "AI Enthusiast"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+
+        count++;
+        index = 0;
+
+        setTimeout(type,1500);
+
+    }else{
+
+        setTimeout(type,100);
+
+    }
+
+})();
