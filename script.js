45cbce92b23dddcb480065fadc07da5ec05da569
Shaik Ayesha <shaikayesha17865@gmail.com>
2026-07-09 21:28:51 +0530

    Implement 'Back To Top' button functionality
    Add functionality for a 'Back To Top' button that appears after scrolling down.

--- script.js
+++ script.js
@@ -52,3 +52,25 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+// Back To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+};
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+};
